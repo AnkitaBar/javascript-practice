e599a166ef6713d926599499cc08b9455129c743
Ankita <ankita.bar1026@gmail.com>
2023-09-25 22:25:37 +0530

    js

---
+++ 05_practiceset1.js
@@ -0,0 +1,35 @@
+//ch1 Q1
+
+let a="Ankita"
+let b=6
+console.log(a+b)
+
+//Q2
+
+console.log(typeof a+b)
+
+//Q3
+
+const a1 = {
+    name:"ankita",
+    section: 1,
+    isPrincipal:false
+}
+
+// a1=45 not hold number 
+
+//Q4
+a1['friend'] = "sabarni" 
+a1['name'] = "mantrit"
+console.log(a1)
+
+//Q5
+
+const dict = {
+    surprise : "an unexpected or astonishing event, fact, etc.",
+    
+    yakka : "work, especially of a strenuous physical kind."
+}
+
+console.log(dict['yakka']) 
+console.log(dict.surprise)
